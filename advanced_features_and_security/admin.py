039b30ffd147aa94ffe8a45212e1c7ee85ca738f
rozthegray <rozthegrey@gmail.com>
2025-03-09 10:28:49 -0700

    Added advanced_features_and_security project with custom user model

---
+++ advanced_features_and_security/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "username", "date_of_birth", "is_staff", "is_superuser")
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        ("Personal Info", {"fields": ("date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "date_of_birth", "profile_photo", "password1", "password2", "is_staff", "is_superuser"),
+        }),
+    )
+    search_fields = ("email", "username")
+    ordering = ("email",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
