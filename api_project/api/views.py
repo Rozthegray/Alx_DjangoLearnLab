2248b755ceb1e586d4e6e581ace632e06e21fd59
Rozthegray <rozthegrey@gmail.com>
2025-03-15 07:39:19 -0700

    Setup Django REST Framework with Book model API

---
+++ api_project/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Book
+from .serializers import BookSerializer
+
+@api_view(['GET'])
+def book_list(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
