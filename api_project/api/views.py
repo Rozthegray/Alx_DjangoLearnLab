f2f630aea908b3a991785e22388b19fe2d03b6f7
Rozthegray <rozthegrey@gmail.com>
2025-03-15 07:44:36 -0700

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,7 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
-@api_view(['GET'])
-def book_list(request):
-    books = Book.objects.all()
-    serializer = BookSerializer(books, many=True)
-    return Response(serializer.data)
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
