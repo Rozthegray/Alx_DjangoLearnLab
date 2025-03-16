464fc48d7535209c6618c54aa93c6f2694adf5f5
Rozthegray <rozthegrey@gmail.com>
2025-03-16 08:36:07 -0700

    Update serializers.py

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
-from .models import Author
+from .models import Author, Book
 
 class AuthorSerializer(serializers.ModelSerializer):
+    books = serializers.StringRelatedField(many=True, read_only=True)  # Shows book titles in Author API
+
     class Meta:
         model = Author
-        fields = '__all__'
-
-from .models import Book
+        fields = ['id', 'name', 'birth_date', 'books']
 
 class BookSerializer(serializers.ModelSerializer):
+    author_name = serializers.CharField(source='author.name', read_only=True)  # Include author's name
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ['id', 'title', 'author', 'author_name', 'genre']
