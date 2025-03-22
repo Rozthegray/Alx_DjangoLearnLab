9cc08a17874e7e5b972876a8577996c4e9ff46ea
Rozthegray <rozthegrey@gmail.com>
2025-03-22 04:45:27 -0700

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,59 +1,51 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
-# Create your views here.
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm
-from .forms import RegisterForm
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+# List all blog posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  # blog/templates/blog/post_list.html
+    context_object_name = 'posts'
+    ordering = ['-created_at']
 
-# Register View
-def register(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # Log in the user after registration
-            messages.success(request, "Registration successful.")
-            return redirect('home')
-    else:
-        form = RegisterForm()
-    return render(request, 'blog/register.html', {'form': form})
+# View a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'  # blog/templates/blog/post_detail.html
 
-# Login View
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            messages.success(request, "Logged in successfully.")
-            return redirect('home')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'blog/login.html', {'form': form})
+# Create a new post (Only logged-in users)
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'  # blog/templates/blog/post_form.html
+    fields = ['title', 'content']
 
-# Logout View
-def logout_view(request):
-    logout(request)
-    messages.info(request, "Logged out successfully.")
-    return redirect('home')
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Assign logged-in user as the author
+        return super().form_valid(form)
 
-# Profile View (Only for authenticated users)
-@login_required
-def profile(request):
-    return render(request, 'blog/profile.html', {'user': request.user})
+# Update an existing post (Only the author can edit)
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
 
-@login_required
-def edit_profile(request):
-    if request.method == 'POST':
-        form = ProfileUpdateForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Profile updated successfully.")
-            return redirect('profile')
-    else:
-        form = ProfileUpdateForm(instance=request.user)
-    return render(request, 'blog/edit_profile.html', {'form': form})
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Only the author can edit
+
+# Delete a post (Only the author can delete)
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Only the author can delete
