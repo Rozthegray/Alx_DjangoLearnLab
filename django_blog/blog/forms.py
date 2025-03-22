d79ae12e2f9566957796187a226e26ee14c610e6
Rozthegray <rozthegrey@gmail.com>
2025-03-22 04:39:46 -0700

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -8,3 +8,8 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
