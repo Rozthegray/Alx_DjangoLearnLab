31b0a44db05d848a064129d22649245ce86b7f99
Rozthegray <rozthegrey@gmail.com>
2025-03-22 05:08:41 -0700

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,28 +1,25 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
-from django.views.generic import UpdateView, DeleteView
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Comment
 from .forms import CommentForm
 
-@login_required
-def add_comment(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.author = request.user
-            comment.save()
-            return redirect("post-detail", pk=post_id)
-    else:
-        form = CommentForm()
-    return render(request, "blog/add_comment.html", {"form": form})
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/add_comment.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()  # Redirect to the post detail page
 
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
-    fields = ["content"]
+    form_class = CommentForm
     template_name = "blog/edit_comment.html"
 
     def form_valid(self, form):
@@ -36,7 +33,9 @@ class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
     template_name = "blog/delete_comment.html"
-    success_url = "/"
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()  # Redirect to the post detail page
 
     def test_func(self):
         comment = self.get_object()
