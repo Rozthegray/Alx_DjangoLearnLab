09b180b3b2b1c13907290e1492c5d67a7c19c16e
Rozthegray <rozthegrey@gmail.com>
2025-03-22 04:46:42 -0700

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Post
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -13,3 +14,8 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
