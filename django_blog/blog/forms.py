48b619288ccd86e6b1213de9f34c0ef3407d97ae
Rozthegray <rozthegrey@gmail.com>
2025-03-22 05:08:08 -0700

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,4 @@
 from django import forms
-from .models import Post
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
@@ -10,19 +6,8 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ['content']
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email']
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content']
+    def clean_content(self):
+        content = self.cleaned_data.get("content")
+        if len(content) < 5:
+            raise forms.ValidationError("Comment must be at least 5 characters long.")
+        return content
