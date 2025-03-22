f6805e32759cddd321769964291f3bd1611aee60
Rozthegray <rozthegrey@gmail.com>
2025-03-22 05:00:30 -0700

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,16 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.timezone import now
 
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+class Comment(models.Model):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=now)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return f"/posts/{self.id}/"
+        return f"Comment by {self.author} on {self.post}"
