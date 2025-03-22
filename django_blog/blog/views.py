ee1b4cd7241bc65e30ca17035894f09ad7183260
Rozthegray <rozthegrey@gmail.com>
2025-03-22 04:39:10 -0700

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -44,3 +44,16 @@ def logout_view(request):
 @login_required
 def profile(request):
     return render(request, 'blog/profile.html', {'user': request.user})
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully.")
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+    return render(request, 'blog/edit_profile.html', {'form': form})
+
