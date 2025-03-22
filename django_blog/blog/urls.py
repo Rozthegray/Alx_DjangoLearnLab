6d12019023ee582d9dd1cdf604cda9fa3c5e0dec
Rozthegray <rozthegrey@gmail.com>
2025-03-22 05:17:27 -0700

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,6 +5,14 @@ from .views import (
     CommentDeleteView,
 )
 
+from django.urls import path
+from .views import search_posts
+
+urlpatterns = [
+    path('search/', search_posts, name='search-posts'),
+]
+
+
 urlpatterns = [
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='update-comment'),
