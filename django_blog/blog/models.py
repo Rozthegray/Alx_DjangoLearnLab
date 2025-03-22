8120d875a9b3ee21e461fd76adeeab792f142215
Rozthegray <rozthegrey@gmail.com>
2025-03-22 04:44:30 -0700

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=now)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return f"/posts/{self.id}/"
