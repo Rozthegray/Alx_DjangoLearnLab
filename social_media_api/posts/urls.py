ee989115d6a637fef9c1ff03dedf446986841bcb
Rozthegray <rozthegrey@gmail.com>
2025-03-30 15:59:47 -0700

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,16 +1,3 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from .models import Post
-from .serializers import PostSerializer
-
-class FeedView(generics.ListAPIView):
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        user = self.request.user
-        return Post.objects.filter(author__in=user.following.all()).order_by('-created_at')
-
 from django.urls import path
 from .views import UserFeedView
 
