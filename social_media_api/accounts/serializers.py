f456e354805ab6ba9ec913e3217d3dfb2cab9d32
Rozthegray <rozthegrey@gmail.com>
2025-03-30 13:43:35 -0700

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,17 +5,18 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)  # Ensure password is hidden in responses
 
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture', 'followers']
 
     def create(self, validated_data):
+        # Create a new user using the default create_user() method
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password']
         )
-        Token.objects.create(user=user)  # Automatically generate a token
+        Token.objects.create(user=user)  # Generate an auth token for the user
         return user
