f897e239882357d7c2c14cd7628e5040c6befd70
Rozthegray <rozthegrey@gmail.com>
2025-04-04 08:42:24 -0700

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -4,7 +4,20 @@ from django.db import models
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
+from django.db import models
+from django.conf import settings
+
 User = get_user_model()
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')  # Prevent multiple likes from the same user
+
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
