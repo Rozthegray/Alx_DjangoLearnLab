f3f914b9603a979495364f1fa15136d3213c8133
Rozthegray <rozthegrey@gmail.com>
2025-04-04 09:35:23 -0700

    Update urls.py

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -1,1 +1,9 @@
 
+# social_media_api/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('posts.urls')),  # Include posts app URLs
+]
