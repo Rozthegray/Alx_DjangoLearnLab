0e94760ddd0c263464f46e1ba1450d09f2267410
Rozthegray <rozthegrey@gmail.com>
2025-04-04 08:57:03 -0700

    Added notifications app and implemented notifications system

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,21 +1,38 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
-from .models import Post
-from accounts.models import CustomUser  # Import CustomUser model
+from .models import Post, Like
+from notifications.models import Notification
+from django.contrib.contenttypes.models import ContentType
 
-class FeedView(generics.GenericAPIView):
+class LikePostView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, request):
-        # Get the list of users the current user follows
-        following_users = request.user.following.all()
+    def post(self, request, pk):
+        post = get_object_or_404(Post, id=pk)
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
 
-        # Retrieve posts from followed users, ordered by creation date (newest first)
-        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+        if created:
+            # Create a notification
+            Notification.objects.create(
+                recipient=post.author,
+                actor=request.user,
+                verb="liked your post",
+                target=post,
+            )
+            return Response({"message": "Post liked!"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "You already liked this post!"}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Serialize the posts (assuming you have a PostSerializer)
-        from .serializers import PostSerializer
-        serializer = PostSerializer(posts, many=True)
+class UnlikePostView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-        return Response(serializer.data)
+    def post(self, request, pk):
+        post = get_object_or_404(Post, id=pk)
+        like = Like.objects.filter(user=request.user, post=post)
+
+        if like.exists():
+            like.delete()
+            return Response({"message": "Like removed!"}, status=status.HTTP_200_OK)
+        return Response({"message": "You haven't liked this post!"}, status=status.HTTP_400_BAD_REQUEST)
