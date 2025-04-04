6ef257aae59a30b71e5aa0f69b4660fb47937400
Rozthegray <rozthegrey@gmail.com>
2025-04-04 09:45:16 -0700

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -3,9 +3,6 @@ from django.urls import path
 from .views import LikePostView, UnlikePostView
 
 urlpatterns = [
-    # Like post
     path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
-
-    # Unlike post
     path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
