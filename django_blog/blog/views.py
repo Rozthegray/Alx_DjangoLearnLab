b1abc41dd1ca8648329f8c1352780e5bfd6c97bb
Rozthegray <rozthegrey@gmail.com>
2025-04-06 17:15:39 -0700

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully.")
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+    return render(request, 'blog/edit_profile.html', {'form': form})
