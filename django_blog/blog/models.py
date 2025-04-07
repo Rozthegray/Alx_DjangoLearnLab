56642f0553bf14ff1eb62c904e1255bc38fcd0b7
Rozthegray <rozthegrey@gmail.com>
2025-04-06 17:26:32 -0700

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -12,3 +12,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+        
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return f"/posts/{self.id}/"
