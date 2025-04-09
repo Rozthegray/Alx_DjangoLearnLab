cc2f29c4df7c5c7149d4620fab1a4875dee4fe26
Rozthegray <rozthegrey@gmail.com>
2025-04-09 13:37:17 -0700

    Update views.py

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -58,3 +58,19 @@ class NotificationUnreadView(APIView):
         notification.save()
 
         return Response({"detail": "Notification marked as unread."})
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_notifications(request):
+    notifications = Notification.objects.filter(recipient=request.user).order_by('-timestamp')
+    data = [
+        {
+            "actor": n.actor.username,
+            "verb": n.verb,
+            "target_id": n.target_id,
+            "timestamp": n.timestamp,
+            "read": n.read,
+        }
+        for n in notifications
+    ]
+    return Response(data)
