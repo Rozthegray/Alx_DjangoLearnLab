203a1dc86b6f3a52ccefaf4c1dcc7c081c383dd4
Rozthegray <rozthegrey@gmail.com>
2025-04-09 02:59:05 -0700

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
