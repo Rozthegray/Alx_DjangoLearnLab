948e5175f97ff067dfd99261ba8a5a8663a99a72
Rozthegray <rozthegrey@gmail.com>
2025-04-09 13:34:21 -0700

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.id}"
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=255)
