abecf208c093c9d69d8bc2f13779a70b6f0cc7c6
Rozthegray <rozthegrey@gmail.com>
2025-04-09 08:14:01 -0700

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,18 +1,24 @@
-from django.contrib.auth import get_user_model
+# accounts/serializers.py
+
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    followers_count = serializers.SerializerMethodField()
-    following_count = serializers.SerializerMethodField()
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'bio', 'profile_picture', 'followers_count', 'following_count']
+        fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture')
 
-    def get_followers_count(self, obj):
-        return obj.followers.count()
-
-    def get_following_count(self, obj):
-        return obj.following.count()
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', '')
+        )
+        Token.objects.create(user=user)
+        return user
