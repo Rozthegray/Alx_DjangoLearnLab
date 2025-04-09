237621281b7f7c3a2a63ca30ca39394009aedc7b
Rozthegray <rozthegrey@gmail.com>
2025-04-09 08:35:55 -0700

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -52,3 +52,29 @@ def unlike_post(request, pk):
     like.delete()  # Delete the like
 
     return Response({"detail": "Post unliked."
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Allow read-only for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Only allow authors to modify
+        return obj.author == request.user
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
