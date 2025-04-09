b95fe08c004a14ed876c8bd388049a5e92263ba4
Rozthegray <rozthegrey@gmail.com>
2025-04-09 08:40:20 -0700

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -10,3 +10,8 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
